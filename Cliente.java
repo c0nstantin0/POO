6c8b57295e8c6beacbc84dd4f8e3bf40a265c77f
c0nstantin0 <lv22338@gmail.com>
2024-09-07 20:10:49 -0300

    Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,23 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    // Construtor
+    public Cliente(int id, String nome, String endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+}
